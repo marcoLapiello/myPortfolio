7ee5bee6d4d62d169856c3042d4ef65999e5f308
marcoLapiello <marcolapiello@gmail.com>
2024-12-05 12:14:33 +0100

    Contacts - check logic added

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+
+  isChecked = false;
+
+  checkUncheck(){
+    if (!this.isChecked) {
+      this.isChecked = true;
+    } else {
+      this.isChecked = false;
+    }
+  }
 
 }
