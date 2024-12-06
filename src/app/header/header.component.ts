38d8a4f55da8ce0c42e43d2cac4c15b3973c355b
marcoLapiello <marcolapiello@gmail.com>
2024-12-06 12:48:43 +0100

    Header - switch language - logic to update the class of the img container to start the switch language function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  language = "english";
+
+  switchLanguage() {
+    if (this.language === "english") {
+      this.language = "german";
+    } else {
+      this.language = "english";
+    }
+    
+  }
 
 }
