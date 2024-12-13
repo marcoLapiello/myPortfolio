a675897d2403b7022e23e5fa53f7751ca01cf068
marcoLapiello <marcolapiello@gmail.com>
2024-12-13 17:35:45 +0100

    Form validation enhanced

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
@@ -12,9 +12,13 @@ import { RouterModule } from '@angular/router';
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+  @ViewChild('formElement') formElement!: ElementRef;
+  @ViewChild('contactForm') contactForm!: NgForm;
 
+  formSubmitted = false;
   isChecked = false;
   showPrivacyPolicyError = false;
+  formSentConfirmation = false;
   mailTest = true;
 
   contactData = {
@@ -22,8 +26,6 @@ export class ContactMeComponent {
     email: "",
     message: "",
   }
-
-  
 
   http = inject(HttpClient);
 
@@ -38,7 +40,7 @@ export class ContactMeComponent {
     },
   };
 
-  checkUncheck(){
+  checkUncheck() {
     if (!this.isChecked) {
       this.isChecked = true;
     } else {
@@ -47,14 +49,15 @@ export class ContactMeComponent {
   }
 
   onSubmit(ngForm: NgForm) {
+    this.formSubmitted = true;
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest && this.isChecked) {
       console.log("Fine");
-      
+
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
           next: (response) => {
-            
-            
+
+
             ngForm.resetForm();
           },
           error: (error) => {
@@ -63,15 +66,67 @@ export class ContactMeComponent {
           complete: () => console.info('send post complete'),
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest && this.isChecked) {
-      console.log("sent" , this.contactData);
-      this.isChecked = false;
-      this.showPrivacyPolicyError = false;
-      ngForm.resetForm();
+      console.log("Sent in test-mode", this.contactData);
+      this.formSentConfirmation = true;
+      this.resetFormState(ngForm);
+      setTimeout(() => {
+        this.formSentConfirmation = false;
+      }, 3000);
+    } else if (this.isFormEmpty()) {
+      this.showGeneralError(ngForm);
     } else {
-      ngForm.form.markAllAsTouched();
-      this.showPrivacyPolicyError = true;
-      console.log("error");
+      this.handleFormErrors();
     }
+  }
+
+  isFormEmpty(): boolean {
+    return Object.values(this.contactData).every(value => (value || "").trim() === "");
+  }
+
+  showGeneralError(ngForm: NgForm): void {
+    this.showPrivacyPolicyError = !this.isChecked;
+    console.log("Form is empty. Showing general error.");
+    setTimeout(() => {
+      this.resetFormState(ngForm);
+    }, 3000);
+  }
+
+  handleFormErrors(): void {
+    this.showPrivacyPolicyError = !this.isChecked;
+    console.log("Form has validation errors.");
+  }
+
+  resetFormState(ngForm: NgForm): void {
+    this.formSubmitted = false;
+    this.isChecked = false;
+    this.showPrivacyPolicyError = false;
+    ngForm.resetForm();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent): void {
+    const clickedInside = this.formElement.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.formSubmitted = false;
+      this.resetFormErrors(this.contactForm);
+    }
+  }
+
+  resetFormErrors(ngForm: NgForm): void {
+    const controls = ngForm.controls;
+    this.showPrivacyPolicyError = false;
+    if (controls) {
+      Object.keys(controls).forEach((key) => {
+        const control = controls[key];
+        if (control) {
+          control.markAsPristine();
+        }
+      });
+    }
+  }
+
+  confirmationFeedback(){
+
   }
 
 }
