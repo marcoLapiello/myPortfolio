2be0a47ca27e2b3fb545720f01842c69aa7bf7ec
marcoLapiello <marcolapiello@gmail.com>
2024-12-16 11:46:25 +0100

    Translation service configured - only working in header till now

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, HttpClient, withInterceptors } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([])),
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }).providers!
   ]
 };
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, CommonModule, DropdownMenuComponent],
+  imports: [RouterModule, CommonModule, DropdownMenuComponent, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -13,11 +14,19 @@ export class HeaderComponent {
   language = "english";
   showModal = false;
 
+  constructor (private translate: TranslateService){
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
   switchLanguage() {
     if (this.language === "english") {
       this.language = "german";
+      this.translate.use('de');
+      
     } else {
       this.language = "english";
+      this.translate.use('en');
     }
     
   }
