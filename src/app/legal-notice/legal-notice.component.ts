65a49ea0b3f6bbb71c08f2a1b6e4e04845eda973
marcoLapiello <marcolapiello@gmail.com>
2024-12-17 16:51:00 +0100

    Privacy policy added and translated

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../translation.service';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+  currentLanguage!: string;
+
+  constructor(private translationService: TranslationService){
+    this.translationService.currentLanguage$.subscribe((language) => {
+      this.currentLanguage = language;
+    });
+  }
 
 }
