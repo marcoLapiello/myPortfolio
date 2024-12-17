c0aa9e30b775adaa694d42c2cfe2b0ac6d9d5766
marcoLapiello <marcolapiello@gmail.com>
2024-12-17 12:32:16 +0100

    Imprint get now dynamically translated

--- src/app/imprint/imprint/imprint.component.ts
+++ src/app/imprint/imprint/imprint.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../translation.service';
 
 @Component({
   selector: 'app-imprint',
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
   emailAddress = "marcolapiello@gmail.com";
+  currentLanguage!: string;
+
+  constructor(private translationService: TranslationService){
+    this.translationService.currentLanguage$.subscribe((language) => {
+      this.currentLanguage = language;
+    });
+  }
 
   sendEmail() {
     window.location.href = `mailto:${this.emailAddress}`;
