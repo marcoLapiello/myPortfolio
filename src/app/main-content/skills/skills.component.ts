3a93b00d40736c275041312944f3b9089ecbc847
marcoLapiello <marcolapiello@gmail.com>
2024-12-18 16:02:44 +0100

    Growth Mindset element is now ready

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, inject } from '@angular/core';
 import { ScrollAndLinkService } from '../../scroll-and-links.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../translation.service';
@@ -10,11 +10,51 @@ import { TranslationService } from '../../translation.service';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-  constructor(private scrollAndLinkService: ScrollAndLinkService) {}
+  growthMindsetOpen = false;
+  growthMindsetPulse = true;
+
+
+  constructor(
+    private scrollAndLinkService: ScrollAndLinkService,
+    private elementRef: ElementRef
+  ) { }
 
   translate = inject(TranslationService);
 
-  scrollToSection(sectionId:string): void {
+  scrollToSection(sectionId: string): void {
     this.scrollAndLinkService.scrollToSection(sectionId);
   }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const clickedTarget = event.target as HTMLElement;
+    const container = this.elementRef.nativeElement.querySelector('#growthMindsetContainer');
+    const speechBubble = this.elementRef.nativeElement.querySelector('#speechBubble');
+    if (
+      container &&
+      speechBubble &&
+      !container.contains(clickedTarget) &&
+      !speechBubble.contains(clickedTarget) &&
+      this.growthMindsetOpen
+    ) {
+      this.closeGrowthMindset();
+    }
+  }
+
+  showGrowthMindset(event: MouseEvent): void {
+    if (window.matchMedia('(hover: none)').matches) {
+      if (this.growthMindsetOpen) {
+        this.closeGrowthMindset();
+      } else {
+        this.growthMindsetOpen = true;
+        this.growthMindsetPulse = false;
+      }
+    }
+  }
+
+  closeGrowthMindset(): void {
+    this.growthMindsetOpen = false;
+    this.growthMindsetPulse = true;
+  }
 }
+
